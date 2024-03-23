2aa8b4a542cbbf7a9ac84cfd11815b68e5e6fbba
cmy3n <alexandr_stepanov00@mail.ru>
2024-03-23 11:01:40 +0300

    first commit

---
+++ Praktika3_2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika3_2
+{
+    internal class Class1
+    {
+        public int ID_Client;
+        public int ID_OrderClients;
+
+        public string ClientSurname { get; private set; }
+        public string ClientName { get; private set; }
+        public string ClientMiddleName { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+
+
+        public string Products { get; private set; }
+
+
+        public DateTime Receiving_DateTime { get; private set; }
+
+
+        public Class1(OrdersClients oc)
+        {
+            Receiving_DateTime = oc.ReceivingDateTime;
+
+            Products = oc.Orders.Products;
+
+            ClientSurname = oc.Clients.ClientSurname;
+            ClientName = oc.Clients.ClientName;
+            ClientMiddleName = oc.Clients.ClientMiddleName;
+            PhoneNumber = oc.Clients.PhoneNumber;
+            Email = oc.Clients.Email;
+        }
+
+    }
+}
---
+++ Praktika3_2/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktika3_2
+{
+    public partial class MainWindow : Window
+    {
+        private PunktVidachiZakazovEntities pvz = new PunktVidachiZakazovEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataGrid1.ItemsSource = pvz.OrdersClients.ToList();
+        }
+    }
+}
